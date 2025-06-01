7f93eecf870b3a7e01045682081c0c11f85e9ad9
Alaoscorty <133812869+Alaoscorty@users.noreply.github.com>
2025-06-01 22:42:41 +0100

    new

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: '#3B82F6',
+                        secondary: '#1E40AF',
+                        dark: '#1F2937',
+                        light: '#F3F4F6',
+                    },
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                    },
+                }
+            }
+}
